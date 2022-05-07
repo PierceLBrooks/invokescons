fb15b6575fdef5e1312b5dfb6928a0442738c6b1
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2022-05-07 14:52:47 -0400

    actually handle command arguments like "--help"

--- invokescons/command_line.py
+++ invokescons/command_line.py
@@ -6,11 +6,19 @@ def main():
     arguments = sys.argv
     command = []
     if (len(arguments) > 1):
+        arguments = arguments[1:]
+        for i in range(len(arguments)):
+            argument = arguments[i]
+            arguments[i] = "arguments.append(\""+argument+"\")"
+        if (len(arguments) > 1):
+            arguments = "; ".join(arguments)
+        else:
+            arguments = arguments[0]+"; "
         command.append(sys.executable)
         command.append("-c")
-        command.append("\"import SCons.Script; SCons.Script.main()\"")
-        command += arguments[1:]
-        result = subprocess.check_output(command)
+        command.append("import sys; import SCons.Script; arguments = []; "+arguments+"sys.argv += arguments; print(str(sys.argv)); SCons.Script.main()")
+        print(str(command))
+        result = subprocess.check_output(command, stderr=subprocess.STDOUT)
         print(result.decode())
     else:
         SCons.Script.main()
